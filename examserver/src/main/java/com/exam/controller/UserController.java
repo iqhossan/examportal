b1564280b3d612d39b2cc4c2045e27fdf3cae9dc
iqhossan <hossan.iqbal@gmail.com>
2024-01-23 20:21:05 -0600

    updated the changes

--- examserver/src/main/java/com/exam/controller/UserController.java
+++ examserver/src/main/java/com/exam/controller/UserController.java
@@ -24,6 +24,12 @@ public class UserController {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/test")
+    public String test(){
+        return "Welcome to Backend API of Exam portal";
+    }
+
     //creating user
     @PostMapping("/")
     public User createUser(@RequestBody User user) throws Exception {
--- examserver/src/main/java/com/exam/model/exam/Quiz.java
+++ examserver/src/main/java/com/exam/model/exam/Quiz.java
@@ -14,6 +14,7 @@ public class Quiz {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long qId;
     private String title;
+    @Column(length=5000)
     private String description;
     private String maxMarks;
     private String numberOfQuestions;
